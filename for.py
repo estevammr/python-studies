868d541899d3ed26272a3a41c59c8895863b4b33
Estevam Rodrigues <estevam.mrodrigues@gmail.com>
2018-03-17 21:40:21 -0300

    For

---
+++ for.py
@@ -0,0 +1,13 @@
+list_numbers = [0, 2, 5, 12, 152]
+
+for number in list_numbers[0:3]:
+    if number > 2:
+        print(number)
+
+list_names = ["Estevam", "Rodrigues"]
+
+for name in list_names:
+    if name == "Estevam":
+        print(name)
+
+
