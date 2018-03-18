dc331bf2e002a02a59f73d000076893e80950324
Estevam Rodrigues <estevam.mrodrigues@gmail.com>
2018-03-17 22:31:08 -0300

    Functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+def call_numbers():
+    for i in range(0, 3):
+        print("Call numbers: " + str(i))
+
+
+def call_numbers_with_limit(limit):
+    for i in range(0, limit):
+        print("Call numbers with limit: " + str(i))
+
+
+call_numbers()
+call_numbers_with_limit(5)
+
