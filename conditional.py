490c3dcfdd71b5b1151a1223ade48c20f9815104
Estevam Rodrigues <estevam.mrodrigues@gmail.com>
2018-03-17 21:12:19 -0300

    Conditional

---
+++ conditional.py
@@ -0,0 +1,14 @@
+def conditional():
+
+    total = 11
+
+    if total > 10:
+        print("Greater than 10")
+    elif total < 10:
+        print("Other condition")
+    else:
+        print("What?")
+
+
+if __name__ == '__main__':
+    conditional()
