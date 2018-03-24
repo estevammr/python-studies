2104dad7f05bea2c2adbc26149e0106a3f387f56
Estevam Rodrigues <estevam.mrodrigues@gmail.com>
2018-03-24 20:14:38 -0300

    Exercise

--- scripts/file.py
+++ scripts/file.py
@@ -10,3 +10,11 @@ files.close()
 
 with open('/home/www/python-studies/scripts/file.txt', 'a') as files:
     files.write('\nThe with method.')
+
+'''Exercise'''
+
+ids = ['B3', '\nB4', '\nB5', '\nB6']
+
+with open('/home/www/python-studies/scripts/testing.txt', 'w') as files:
+    for item in ids:
+        files.write(item)
