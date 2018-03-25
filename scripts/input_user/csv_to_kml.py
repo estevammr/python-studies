bef4eac802ecc703957abf7fe03483b1710c67a0
Estevam Rodrigues <estevam.mrodrigues@gmail.com>
2018-03-25 12:53:25 -0300

    Gui tkinter

--- scripts/input_user/csv_to_kml.py
+++ scripts/input_user/csv_to_kml.py
@@ -1,16 +1,35 @@
 import simplekml
 import pandas
+import tkinter
+from tkinter.filedialog import askopenfilename
 
 
-df = pandas.read_csv("/home/www/python-studies/scripts/input_user/coordinates.csv")
-kml = simplekml.Kml()
+def browse():
+    global infile
+    infile = askopenfilename()
 
-for lon, lat in zip(df["Longitude"], df["Latitude"]):
 
-    kml.newpoint(coords=[(lon, lat)])
-kml.save("/home/www/python-studies/scripts/input_user/kml_test_input_2.kml")
-print("File created, input in google earth for view point.")
+def kml_gui(outfile='/home/www/python-studies'
+                    '/scripts/input_user'
+                    '/kml_test_input_2.kml'):
+    df = pandas.read_csv(infile)
+    kml = simplekml.Kml()
 
+    for lon, lat in zip(df["Longitude"], df["Latitude"]):
+        kml.newpoint(coords=[(lon, lat)])
+    kml.save(outfile)
+    print("File created, input in google earth for view point.")
+
+
+root = tkinter.Tk()
+root.title("Kml generator")
+label = tkinter.Label(root, text="This program generate a KML file")
+label.pack()
+browseButton = tkinter.Button(root, text="Browse", command=browse)
+browseButton.pack()
+kmlButton = tkinter.Button(root, text="Generate KML", command=kml_gui)
+kmlButton.pack()
+root.mainloop()
 
 
 
